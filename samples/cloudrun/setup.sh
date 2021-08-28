3c6e9cb14b3b0a0dc730a69116b330225ce74292
Wenlei (Frank) He <wlhe@google.com>
2021-08-27 22:55:57 -0700

    add notes when trying the README.

--- samples/cloudrun/setup.sh
+++ samples/cloudrun/setup.sh
@@ -4,16 +4,24 @@ export PROJECT_ID=${PROJECT_ID:-wlhe-cr}
 export CLUSTER_LOCATION=${CLUSTER_LOCATION:-us-central1-c}
 export CLUSTER_NAME=${CLUSTER_NAME:-asm-cr}
 export REGION=${REGION:-us-central1}
+export WORKLOAD_NAMESPACE=fortio 
+export WORKLOAD_NAME=cloudrun
+export WORKLOAD_SERVICE_ACCOUNT=k8s-${WORKLOAD_NAMESPACE}@${PROJECT_ID}.iam.gserviceaccount.com
+export CLOUDRUN_SERVICE=${WORKLOAD_NAME}
+
+# wlhe@: golden image needs to push to public repo
+export  GOLDEN_IMAGE=gcr.io/wlhe-cr/krun:main
+# Target image 
+export  IMAGE=gcr.io/${PROJECT_ID}/fortio-cr:main
 
 export NS=${NS:-fortio}
 
 # Example command to create a regular cluster.
 function create_cluster() {
-
   gcloud beta container --project "${PROJECT_ID}" clusters create \
     "${CLUSTER_NAME}" --zone "${CLUSTER_LOCATION}" \
     --no-enable-basic-auth \
-    --cluster-version "1.20.8-gke.700" \
+    --cluster-version "1.21.3-gke.2000" \
     --release-channel "rapid" \
     --machine-type "e2-standard-8" \
     --image-type "COS_CONTAINERD" \
@@ -82,8 +90,7 @@ function setup_project() {
 }
 
 
-function setup_namespace() {
-
+function setup_service_account() {
   gcloud --project ${PROJECT_ID} iam service-accounts create k8s-${NS} \
         --display-name "Service account with access to ${NS} k8s namespace"
 
@@ -91,37 +98,43 @@ function setup_namespace() {
               ${PROJECT_ID} \
               --member="serviceAccount:k8s-${NS}@${PROJECT_ID}.iam.gserviceaccount.com" \
               --role="roles/container.clusterViewer"
-
-  cat manifests/rbac.yaml | envsubst | kubectl apply -f -
 }
 
-function setup_fortio() {
+function setup_namespace() {
   gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_LOCATION} --project ${PROJECT_ID}
 
   kubectl create ns fortio
   kubectl label namespace fortio istio-injection- istio.io/rev=asm-managed-rapid --overwrite
+  cat manifests/rbac.yaml | envsubst | kubectl apply -f -
+}
 
+
+# wlhe@: is it needed?
+function setup_fortio() {
   helm upgrade --install \
       -n fortio \
       fortio \
       samples/charts/fortio
 }
 
+# wlhe@: GOLDEN_IMAGE needs to be published in a public repo.
+function build_fortio() {
+  docker build -f samples/fortio/Dockerfile . -t ${IMAGE} --build-arg=BASE=${GOLDEN_IMAGE} 
+  docker push ${IMAGE}
+}
 
 function deploy_app() {
-  export CLOUDRUN_SERVICE=fortio-asm-cr
-  export REGION=us-central1
-
   gcloud alpha run deploy ${CLOUDRUN_SERVICE} \
           --platform managed \
           --project ${PROJECT_ID} \
           --region ${REGION} \
-          --sandbox=minivm \
+          --execution-environment=gen2 \
           --allow-unauthenticated \
           --use-http2 \
           --port 15009 \
           --image ${IMAGE} \
-          --vpc-connector projects/${PROJECT_ID}/locations/${CLOUDRUN_REGION}/connectors/serverlesscon \
+          --vpc-connector projects/${PROJECT_ID}/locations/${REGION}/connectors/serverlesscon \
           --set-env-vars="CLUSTER_NAME=${CLUSTER_NAME}" \
          --set-env-vars="CLUSTER_LOCATION=${CLUSTER_LOCATION}"
 }
+
